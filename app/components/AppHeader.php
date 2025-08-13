8780a1b68f1a1d5b37aa35fead7f038561a8d6ef
鼠子 <165134730+YuiNijika@users.noreply.github.com>
2025-08-14 07:40:21 +0800

    Update AppHeader.php

--- app/components/AppHeader.php
+++ app/components/AppHeader.php
@@ -12,7 +12,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
     <script type="module" src="http://localhost:3000/@vite/client"></script>
     <script type="module" src="http://localhost:3000/src/main.ts"></script>
     <?php } else { ?>
-    <link rel="stylesheet" href="<?php echo get_theme_file_url('assets/dist/components.css?ver=' . get_theme_version(false)); ?>">
+    <link rel="stylesheet" href="<?php get_theme_file_url('assets/dist/components.css?ver=' . get_theme_version(false)); ?>">
     <?php } ?>
 </head>
 
