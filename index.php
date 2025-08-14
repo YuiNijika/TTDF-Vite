54b973739a365b530d9d8126ea82d108bdda9429
鼠子 <165134730+YuiNijika@users.noreply.github.com>
2025-08-14 14:53:21 +0800

    更新 index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
 /**
- * Kasumi Theme for Typecho.
- * @package Kasumi
+ * TTDF Vite
+ * @package TTDF Vite
  * @author 鼠子(Tomoriゞ)
  * @version 1.0.0
- * @link https://github.com/YuiNijika/Kasumi
+ * @link https://github.com/YuiNijika/TTDF
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 Get::Components('AppHeader');
